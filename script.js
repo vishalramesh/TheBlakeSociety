2738ebbc942cf8e61303469e28062c89cffc1544
vishalramesh <vishal-ramesh@outlook.com>
2019-05-27 11:11:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+function gaiman() {
+    document.querySelector("#title").innerHTML = "Patron";
+    document.querySelector("#name").innerHTML = "Neil Gaiman";
+    document.querySelector("#image_selector").setAttribute("src", "images/montages/neil_gaiman.jpg");
+}
+
+function moore() {
+    document.querySelector("#title").innerHTML = "Patron";
+    document.querySelector("#name").innerHTML = "Alan Moore";
+    document.querySelector("#image_selector").setAttribute("src", "images/montages/alan_moore.jpg");
+}
+
+function pullman() {
+    document.querySelector("#title").innerHTML = "President";
+    document.querySelector("#name").innerHTML = "Philip Pullman";
+    document.querySelector("#image_selector").setAttribute("src", "images/montages/philip_pullman.jpg");
+}
+
+
+/*
+function time(i) {
+        setTimeout(pullman(), 3000);
+  
+        setTimeout(gaiman(), 3000);
+
+        setTimeout(moore(), 3000);
+}
+
+time()
+
+var seconds = 0;
+
+function incrementSeconds() {
+    seconds += 1;
+}
+
+setInterval(incrementSeconds, 1000);
+*/
+
